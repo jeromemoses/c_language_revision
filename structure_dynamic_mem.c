da6620060bee6e4e2bfb252d0c36448ac39dfdcc
jeromemoses <jeromemoses05@gmail.com>
2024-02-03 22:02:16 +0530

    MD_2/3/2024

--- structure_dynamic_mem.c
+++ structure_dynamic_mem.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
-
+#include<string.h>
 struct person
 {
     char name[25];
@@ -12,7 +12,8 @@ void main()
     //creating structure pointer and assinging memory dynamically 
     struct person *pr1 = (struct person*)malloc(sizeof(struct person));
 
-    *pr1->name = "jerome";
+    //*pr1->name = "jerome";
+    //strcpy(pr1->age,"jerome");
     pr1->age = 22;
 
     printf("Available data inside the pointer structure address:\nname:%s\t age: %d",pr1->name,pr1->age);
